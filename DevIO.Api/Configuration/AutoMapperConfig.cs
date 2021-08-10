feab5d50e80217ecb45744ca62dd67aad5f3247e
leilagama14 <leilagama@gmail.com>
2021-08-10 10:01:15 -0300

    Add project files.

---
+++ DevIO.Api/Configuration/AutoMapperConfig.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Configuration
+{
+    //herda de profile que é uma classe automapper de perfil
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            //Mapeia de Fornecedor para FornecedorViewModel
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+            //Mapeia de Endereco para EnderecoViewModel
+            CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+            //Mapeia de Produto para ProdutoViewModel
+            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+        }
+
+    }
+}
---
+++ DevIO.Api/Controllers/FornecedoresController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Controllers
+{
+    [Microsoft.AspNetCore.Components.Route("api/fornecedores")]
+    public class FornecedoresController : MainController
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IMapper _mapper;
+
+        public FornecedoresController(IFornecedorRepository fornecedorRepository)
+        {
+            _fornecedorRepository = fornecedorRepository;
+        }
+
+
+        public async Task<IEnumerable<FornecedorViewModel>> ObterTodos()
+        {
+            var fornecedor = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
+
+            return fornecedor;
+        }
+    }
+}
